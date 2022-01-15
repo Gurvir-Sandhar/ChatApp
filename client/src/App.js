bc1211d810312109c88d2efd891575a247b160d2
gurvir-sandhar <gurvirsandhar@gmail.com>
2022-01-14 16:13:56 -0800

    added server and test connected to react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
+import React, { Component } from "react"
 import './App.css';
 import Login from './Login.js'
 import Header from './Header.js'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Login />
-    </div>
-  );
+class App extends React.Component {
+    state = {
+      data: null
+    }
+
+    componentDidMount() {
+        this.callBackendAPI()
+            .then(res => this.setState({data: res.express}))
+            .catch(err => console.log(err));
+    }
+
+    callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
+
+        if(response.status !== 200){
+            throw Error(body.message)
+        }
+        return body;
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <Login />
+            </div>
+        )
+    };
 }
 
 export default App;
